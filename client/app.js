1d29980d5a21b87b4535f7117e726b91a9923a8f
achl93 <andy_ch_009@hotmail.com>
2018-07-21 18:09:59 -0700

    wrap client in jquery document ready

--- client/app.js
+++ client/app.js
@@ -1,28 +1,30 @@
-var socket = io();
+$('document').ready(() => {
+  var socket = io();
 
-function updateScroll() {
-  var messageBody = document.querySelector('.main');
-  messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
-}
-
-$('form').submit(function () {
-  var text = $('#message').val();
-  var initials = $('#initials').val();
-  var message = {
-    initials: initials,
-    text: text
+  function updateScroll() {
+    var messageBody = document.querySelector('.main');
+    messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
   }
-  socket.emit('message', message);
-  $('#message').val('');
-  return false;
-});
-
-socket.on('catchup', (msg) => {
-  $('<li>').text((msg.initials === "" ? 'Unknown' : msg.initials) + ' says: ' + msg.text).appendTo('#history');
-  updateScroll();
+  
+  $('form').submit(() => {
+    var text = $('#message').val();
+    var initials = $('#initials').val();
+    var message = {
+      initials: initials,
+      text: text
+    }
+    socket.emit('message', message);
+    $('#message').val('');
+    return false;
+  });
+  
+  socket.on('catchup', (msg) => {
+    $('<li>').text((msg.initials === "" ? 'Unknown' : msg.initials) + ' says: ' + msg.text).appendTo('#history');
+    updateScroll();
+  })
+  
+  socket.on('message', (msg) => {
+    $('<li>').text((msg.initials === "" ? 'Unknown' : msg.initials) + ' says: ' + msg.text).appendTo('#history');
+    updateScroll();
+  });
 })
-
-socket.on('message', (msg) => {
-  $('<li>').text((msg.initials === "" ? 'Unknown' : msg.initials) + ' says: ' + msg.text).appendTo('#history');
-  updateScroll();
-});
